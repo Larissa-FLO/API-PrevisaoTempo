b2c6e1b32e34697120dd6646dba491f3c8a8f7b5
Larissa-FLO <larissa.arqueo@outlook.com>
2023-12-05 23:05:34 -0300

    ProjetoPrevisaoTempo-APIOpenWeather

---
+++ script.js
@@ -0,0 +1,27 @@
+const key = "e0283766cee7e7e70be7e75409ae8042";
+
+function pesquisar(){
+    let cidade = document.querySelector('.inputCidade').value;
+    buscarDados(cidade);
+}
+
+async function buscarDados(cidade){
+    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Response => Response.json());
+    inserirDados(dados);
+}
+
+function inserirDados(dados){
+    console.log(dados);
+    document.querySelector('.tituloCidade').innerHTML = "Tempo em " + dados.name;
+    document.querySelector('.graus').innerHTML = Math.floor(dados.main.temp) + "°C";
+    document.querySelector('.textoPrevisao').innerHTML = dados.weather[0].description;
+    document.querySelector('.umidade').innerHTML = "Umidade: " + Math.floor(dados.main.humidity) + "%";
+    document.querySelector('.imgPrevisao').src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+}
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        const btn = document.querySelector('.btn');
+        btn.click();
+    }
+})
